dd2ffc229749f664f3131f1fe75754e2fcc47658
Maisha Masnoon <maishamasnoon502@gmail.com>
2026-04-23 12:12:21 -0400

    Initial commit - AI Research Agent with LangChain

---
+++ main.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents import create_tool_calling_agent
+import json
+from tools import search_web, wiki_tool, save_text_to_file
+
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use necessary tool.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+tools = [search_web, wiki_tool, save_text_to_file]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can I help you research?")
+raw_response = agent_executor.invoke({"query": query})
+parsed_ouput = json.loads(raw_response["output"])
+
+print(parsed_ouput)
